444825a78be85f9bf896742efee6df516836caa7
Bakhta Elamar <elamarb@icloud.com>
2018-10-06 12:28:43 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,21 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+        :patriarch => {name: "Lord Montague", age:"53"},
+        :matriarch => {name:"Lady Montague", age:"54"},
+        :hero => {name:"Romeo", age: "15", status: "alive"},
+        :hero_friends => [{name: "Benvolio",age: "17", attitude: "worried"}, {name: "Mercutio",age: "18", attitude: "hot-headed"}]
       #your key/value pairs here!
       }, 
     :capulet => {
+        :patriarch => {name: "Lord Capulet", age of "50"}, 
+        :matriarch => {name of "Lady Capulet", age of "51"}, 
+        :heroine => {name of "Juliet", age of "15", status of "alive"}, 
+        :heroine_friends => [{name: "Steven",age: "30", attitude: "confused"}, {name: "Nurce",age: "44", attitude: "worried"}]
       #your key/value pairs here!
-      }}
+      }
+    
+  }
 
   
 end
